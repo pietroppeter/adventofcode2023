24a5fe9b65d89129daaf6cfc3c22a4d898a90a4b
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-12-01 06:51:25 +0100

    nim fixed day 1 part2

--- day01/solution.nim
+++ day01/solution.nim
@@ -6,17 +6,29 @@ func parse(text: string): int =
   for c in text:
     if c in '0' .. '9':
       onlyDigits.add c
+  if onlyDigits.len == 0:
+    return 0
   let
     first = onlyDigits[0]
     last = onlyDigits[^1]
   (ord(first) - ord('0'))*10 + (ord(last) - ord('0'))
 
+let testInput2 = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+
 var
   part1 = 0
   part2 = 0
+#for line in testInput2.splitLines:
 for line in lines "input.txt":
+  print line
   part1 += parse(line)
-  let processLine = line.multiReplace([
+  let processedLine = line.replace("t", "tt").replace("on", "oon").replace("ei", "eei").multiReplace([
     ("zero", "0"),
     ("one", "1"),
     ("two", "2"),
@@ -28,6 +40,7 @@ for line in lines "input.txt":
     ("eight", "8"),
     ("nine", "9"),
   ])
-  part2 += parse(processLine)
+  print processedLine
+  part2 += parse(processedLine)
 print part1
 print part2
