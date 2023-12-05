840879f4708b1a665a597590c0e8bf9884345dd1
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-12-05 15:15:14 +0100

    day05 python part1 ok

---
+++ day05/solution.py
@@ -0,0 +1,43 @@
+import example
+
+print(example.seeds)
+
+def rangemap(tuples, n):
+  for t in tuples:
+    if t[1] <= n <= t[1] + t[2]:
+      return t[0] + (n - t[1])
+  return n
+
+print(rangemap(example.seed_to_soil_map, 79)) # 81
+print(rangemap(example.soil_to_fertilizer_map, 81)) # 81
+print(rangemap(example.fertilizer_to_water_map, 57)) # 53
+
+def seed_to_location(namespace, n):
+  for m in [
+      namespace.seed_to_soil_map,
+      namespace.soil_to_fertilizer_map,
+      namespace.fertilizer_to_water_map,
+      namespace.water_to_light_map,
+      namespace.light_to_temperature_map,
+      namespace.temperature_to_humidity_map,
+      namespace.humidity_to_location_map,
+    ]:
+    n = rangemap(m, n)
+  return n
+
+print(seed_to_location(example, 79)) # 82
+print(seed_to_location(example, 14)) # 43
+
+def part1(namespace):
+  result = 1_000_000_000
+  for seed in namespace.seeds:
+    n = seed_to_location(namespace, seed)
+    if n < result:
+      result = n
+  return result
+
+print(part1(example)) # 35
+
+import puzzle
+print(part1(puzzle)) # 
+
