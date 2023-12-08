c761cda7e198aa8eae51d054dd53334bc851f6c2
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-12-08 08:17:21 +0100

    nim day07 part2 ok

--- day07/solution.nim
+++ day07/solution.nim
@@ -95,3 +95,72 @@ print part1 hands1
 
 let puzzleHands = "puzzle.txt".readFile.strip.parseHands
 print part1 puzzleHands
+
+
+func val2(c: char): int =
+  case c:
+    of 'J':
+      0
+    of '2' .. '9':
+      ord(c) - ord('0')
+    of 'T':
+      10
+    of 'Q':
+      12
+    of 'K':
+      13
+    of 'A':
+      14
+    else:
+      raise ValueError.newException "unexpected card " & $c
+
+func kind2(h: Hand): handKind =
+  let hand = h.hand
+  assert hand.len == 5
+  var counts = toCountTable[char](hand.toSeq)
+  let jokers = (key: 'J', val: counts['J'])
+  counts.del 'J'
+  var first = if jokers.val == 5: jokers else: largest counts
+  counts.del first.key
+  if jokers.val != 5:
+    first.val += jokers.val
+  var second = if first.val == 5: first else: largest counts
+  
+  if first.val == 5:
+    Five
+  elif first.val == 4:
+    Four
+  elif first.val == 3 and second.val == 2:
+    House
+  elif first.val == 3: # second.val == 1
+    Three
+  elif first.val == 2 and second.val == 2:
+    TwoPairs
+  elif first.val == 2: # second.val == 1
+    Pair
+  else:
+    High
+
+func cmp2(h,k: Hand): int =
+  # h < k -> negative
+  assert h.hand.len == 5
+  assert k.hand.len == 5
+  let
+    hKind = h.kind2
+    kKind = k.kind2
+  result = ord(hKind) - ord(kKind)
+  if result != 0:
+    return result
+  for i in 0 .. 4:
+    result = h.hand[i].val2 - k.hand[i].val2
+    if result != 0:
+      return result
+
+func part2(hands: Hands): int =
+  var hands = hands
+  sort(hands, cmp2)
+  for i, hand in hands:
+    result += (i + 1)* hand.bid
+
+print part2 hands1
+print part2 puzzleHands
